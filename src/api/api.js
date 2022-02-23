a997a5931c67109a02d07f308c02e652b6145ee1
lucas <brunolucasbit@gmail.com>
2022-02-23 16:23:47 -0300

    Update

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,57 @@
 async function ytData(term) {
   const KEY = "";
 
-  const search = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?key=${KEY}&part=snippet&q=${term}&maxResults=5`
+  const page1 = await fetch(
+    `https://www.googleapis.com/youtube/v3/search?key=${KEY}&part=snippet&q=${term}&maxResults=200`
   )
     .then((response) => response.json())
-    .then((data) => {
-      return data.items.map((item) => item.id.videoId);
-    })
+    .then((data) => ({
+      items: data.items.map((item) => item.id.videoId).filter((e) => e),
+      nextPageToken: data.nextPageToken,
+    }))
     .catch((err) => {
       console.log(err);
     });
 
-  const formattedIds = search.toString();
+  console.log(page1);
+
+  const page2 = await fetch(
+    `https://www.googleapis.com/youtube/v3/search?key=${KEY}&part=snippet&pageToken=${page1.nextPageToken}&maxResults=200`
+  )
+    .then((response) => response.json())
+    .then((data) => ({
+      items: data.items.map((item) => item.id.videoId).filter((e) => e),
+      nextPageToken: data.nextPageToken,
+    }));
+
+  console.log(page2);
+
+  const page3 = await fetch(
+    `https://www.googleapis.com/youtube/v3/search?key=${KEY}&part=snippet&pageToken=${page2.nextPageToken}&maxResults=200`
+  )
+    .then((response) => response.json())
+    .then((data) => ({
+      items: data.items.map((item) => item.id.videoId).filter((e) => e),
+      nextPageToken: data.nextPageToken,
+    }));
+
+  console.log(page3);
+
+  const page4 = await fetch(
+    `https://www.googleapis.com/youtube/v3/search?key=${KEY}&part=snippet&pageToken=${page3.nextPageToken}&maxResults=200`
+  )
+    .then((response) => response.json())
+    .then((data) => ({
+      items: data.items.map((item) => item.id.videoId).filter((e) => e),
+      nextPageToken: data.nextPageToken,
+    }));
+
+  console.log(page4);
+
+  const formattedIds = page1.items.toString();
+
+  console.log(formattedIds);
+
   const videos = await fetch(
     `https://www.googleapis.com/youtube/v3/videos?part=id,contentDetails,snippet&id=${formattedIds}&key=${KEY}`
   )
