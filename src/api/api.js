5286f4322ccbb76c3ce9e79f46769e93adafe9dd
lucas <brunolucasbit@gmail.com>
2022-03-08 20:40:26 -0300

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,74 +1,23 @@
-async function ytData(term) {
-  const KEY = "";
+const Key = "";
 
-  const page1 = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?key=${KEY}&part=snippet&q=${term}&maxResults=200`
-  )
-    .then((response) => response.json())
-    .then((data) => ({
-      items: data.items.map((item) => item.id.videoId).filter((e) => e),
-      nextPageToken: data.nextPageToken,
-    }))
-    .catch((err) => {
-      console.log(err);
-    });
-
-  console.log(page1);
-
-  const page2 = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?key=${KEY}&part=snippet&pageToken=${page1.nextPageToken}&maxResults=200`
-  )
-    .then((response) => response.json())
-    .then((data) => ({
-      items: data.items.map((item) => item.id.videoId).filter((e) => e),
-      nextPageToken: data.nextPageToken,
-    }));
-
-  console.log(page2);
-
-  const page3 = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?key=${KEY}&part=snippet&pageToken=${page2.nextPageToken}&maxResults=200`
-  )
-    .then((response) => response.json())
-    .then((data) => ({
-      items: data.items.map((item) => item.id.videoId).filter((e) => e),
-      nextPageToken: data.nextPageToken,
-    }));
-
-  console.log(page3);
-
-  const page4 = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?key=${KEY}&part=snippet&pageToken=${page3.nextPageToken}&maxResults=200`
-  )
-    .then((response) => response.json())
-    .then((data) => ({
-      items: data.items.map((item) => item.id.videoId).filter((e) => e),
-      nextPageToken: data.nextPageToken,
-    }));
-
-  console.log(page4);
-
-  const formattedIds = page1.items.toString();
-
-  console.log(formattedIds);
-
-  const videos = await fetch(
-    `https://www.googleapis.com/youtube/v3/videos?part=id,contentDetails,snippet&id=${formattedIds}&key=${KEY}`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      return data.items.map((item) => ({
-        id: item.id,
-        duration: item.contentDetails.duration,
-        title: item.snippet.title,
-        description: item.snippet.description,
-      }));
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
-  return videos;
+async function Data(term) {
+  const data = [];
+  let token = "";
+  let count = 0;
+  while (data.length < 200) {
+    let url = `https://www.googleapis.com/youtube/v3/search?key=${Key}&part=snippet&q=${term}&maxResults=200`;
+    if (token) {
+      url += `&pageToken=${token}`;
+    }
+    const result = await fetch(url).then((response) => response.json());
+    data.push(...result.items);
+    token = result.nextPageToken;
+    count++;
+  }
+  console.log(count);
+  return data;
 }
 
-export default ytData;
+console.log(Data("cahorro"));
+
+export default Data;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-import ytData from "./api/api";
-import api2 from "./api/api2";
+
